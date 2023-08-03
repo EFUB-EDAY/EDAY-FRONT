7b8a6f8a88563ae80bb5cfffb6a54dbe2549dc19
Yerineee <yerin1103@ewhain.net>
2023-08-03 23:44:31 +0900

    :sparkles: : Info7 터치 영역 추가

--- src/components/infopage/info7/Content2.jsx
+++ src/components/infopage/info7/Content2.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 // assets
@@ -8,14 +8,40 @@ import map2 from '../../../assets/infopage/d7/map2_2.svg';
 import point2 from '../../../assets/infopage/d7/point2_2.svg';
 import map3 from '../../../assets/infopage/d7/map2_3.svg';
 import point3 from '../../../assets/infopage/d7/point2_3.svg';
+import engin1 from '../../../assets/infopage/d7/engin_1.png';
+import engin2 from '../../../assets/infopage/d7/engin_2.png';
+import engin3 from '../../../assets/infopage/d7/engin_3.png';
+import engin4 from '../../../assets/infopage/d7/engin_4.png';
+import engin5 from '../../../assets/infopage/d7/engin_5.png';
+import dorm1 from '../../../assets/infopage/d7/dorm_1.png';
+import dorm2 from '../../../assets/infopage/d7/dorm_2.png';
+import dorm3 from '../../../assets/infopage/d7/dorm_3.png';
+import dorm4 from '../../../assets/infopage/d7/dorm_4.png';
+import dorm5 from '../../../assets/infopage/d7/dorm_5.png';
+import dorm6 from '../../../assets/infopage/d7/dorm_6.png';
+import dorm7 from '../../../assets/infopage/d7/dorm_7.png';
+import pool1 from '../../../assets/infopage/d7/pool_1.png';
+import pool2 from '../../../assets/infopage/d7/pool_2.png';
 
 // components
 import Title from '../../_common/Title';
 import GreenBorder from '../../_common/GreenBorder';
 import InfoContent from '../../_common/InfoContent';
 import Tip from '../../_common/Tip';
+import Img from '../../_common/Img';
 
 const Content2 = () => {
+    const [imgOpen, setImgOpen] = useState(Array(14).fill(false));
+
+    const editImgOpen = (index, value) => {
+        setImgOpen(prevState => {
+            const newState = [...prevState];
+            newState[index] = value;
+            console.log(imgOpen);
+            return newState;
+        });
+    };
+
     return (
         <Wrapper>
             <Title num='1.' title='공학관 올라가기' />
@@ -40,13 +66,63 @@ const Content2 = () => {
             ③ 공대 후문으로 들어가요.`}
             />
             <MapContainer margin='8px'>
-                <object type='image/svg+xml' data={map1}>
+                <MapObject type='image/svg+xml' data={map1}>
                     <MapImg src={map1} width='calc(100% - 6px)' />
-                </object>
+                </MapObject>
                 <MapImg
                     className='point'
                     src={point1}
                     width='calc(100% - 6px)'
+                />
+                <Img
+                    type='info7'
+                    positionX='187px'
+                    positionY='193px'
+                    isImgOpened={imgOpen[0]}
+                    closer={() => editImgOpen(0, false)}
+                    img={engin1}
+                    onClick={() => editImgOpen(0, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='223px'
+                    positionY='180px'
+                    isImgOpened={imgOpen[1]}
+                    closer={() => editImgOpen(1, false)}
+                    img={engin2}
+                    onClick={() => editImgOpen(1, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='223px'
+                    positionY='142.97px'
+                    isImgOpened={imgOpen[2]}
+                    closer={() => editImgOpen(2, false)}
+                    img={engin3}
+                    onClick={() => editImgOpen(2, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='171px'
+                    positionY='113.97px'
+                    isImgOpened={imgOpen[3]}
+                    closer={() => editImgOpen(3, false)}
+                    img={engin4}
+                    onClick={() => editImgOpen(3, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='228px'
+                    positionY='58.97px'
+                    isImgOpened={imgOpen[4]}
+                    closer={() => editImgOpen(4, false)}
+                    img={engin5}
+                    onClick={() => editImgOpen(4, true)}
+                    color='white'
                 />
             </MapContainer>
 
@@ -73,13 +149,83 @@ const Content2 = () => {
             ③ 포스코관 루트를 따라 공대 오르막길을 올라가요.`}
             />
             <MapContainer margin='20px'>
-                <object type='image/svg+xml' data={map2}>
+                <MapObject type='image/svg+xml' data={map2}>
                     <MapImg src={map2} width='calc(100% - 6px)' />
-                </object>
+                </MapObject>
                 <MapImg
                     className='point'
                     src={point2}
                     width='calc(100% - 6px)'
+                />
+                <Img
+                    type='info7'
+                    positionX='203px'
+                    positionY='258px'
+                    isImgOpened={imgOpen[5]}
+                    closer={() => editImgOpen(5, false)}
+                    img={dorm1}
+                    onClick={() => editImgOpen(5, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='274px'
+                    positionY='222px'
+                    isImgOpened={imgOpen[6]}
+                    closer={() => editImgOpen(6, false)}
+                    img={dorm2}
+                    onClick={() => editImgOpen(6, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='280px'
+                    positionY='187px'
+                    isImgOpened={imgOpen[7]}
+                    closer={() => editImgOpen(7, false)}
+                    img={dorm3}
+                    onClick={() => editImgOpen(7, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='195px'
+                    positionY='205px'
+                    isImgOpened={imgOpen[8]}
+                    closer={() => editImgOpen(8, false)}
+                    img={dorm4}
+                    onClick={() => editImgOpen(8, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='248px'
+                    positionY='138px'
+                    isImgOpened={imgOpen[9]}
+                    closer={() => editImgOpen(9, false)}
+                    img={dorm5}
+                    onClick={() => editImgOpen(9, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='150px'
+                    positionY='216px'
+                    isImgOpened={imgOpen[10]}
+                    closer={() => editImgOpen(10, false)}
+                    img={dorm6}
+                    onClick={() => editImgOpen(10, true)}
+                    color='white'
+                />
+                <Img
+                    type='info7'
+                    positionX='179px'
+                    positionY='198px'
+                    isImgOpened={imgOpen[11]}
+                    closer={() => editImgOpen(11, false)}
+                    img={dorm7}
+                    onClick={() => editImgOpen(11, true)}
+                    color='white'
                 />
             </MapContainer>
 
@@ -103,15 +249,40 @@ const Content2 = () => {
                 type='top'
             />
             <MapContainer margin='20px'>
-                <object type='image/svg+xml' data={map3}>
-                    <MapImg src={map3} width='198px' />
-                </object>
+                <MapObject
+                    type='image/svg+xml'
+                    data={map3}
+                    width='198px'
+                    height='193px'
+                >
+                    <MapImg src={map3} width='198px' height='193px' />
+                </MapObject>
                 <MapImg
                     className='point'
                     src={point3}
                     width='54px'
                     top='59px'
-                    left='147px'
+                    left='145px'
+                />
+                <Img
+                    type='info7'
+                    positionX='144px'
+                    positionY='95px'
+                    isImgOpened={imgOpen[12]}
+                    closer={() => editImgOpen(12, false)}
+                    img={pool1}
+                    onClick={() => editImgOpen(12, true)}
+                    color='black'
+                />
+                <Img
+                    type='info7'
+                    positionX='183px'
+                    positionY='59px'
+                    isImgOpened={imgOpen[13]}
+                    closer={() => editImgOpen(13, false)}
+                    img={pool2}
+                    onClick={() => editImgOpen(13, true)}
+                    color='white'
                 />
             </MapContainer>
         </Wrapper>
@@ -135,12 +306,17 @@ const MapContainer = styled.div`
     margin-bottom: ${props => (props.margin ? props.margin : '0px')};
 `;
 
+const MapObject = styled.object`
+    align-self: center;
+`;
+
 const MapImg = styled.img`
     position: relative;
     z-index: 1;
     align-self: center;
 
     width: ${props => props.width};
+    height: ${props => props.height};
 
     &.point {
         position: absolute;
