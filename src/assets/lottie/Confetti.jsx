6485afba39173b4fdaad241396e187263993ab2e
ybchoi5262 <ybchoi5262@ewhain.net>
2023-08-03 15:31:41 +0900

    ✨ : 칭호 모달창 구현

--- src/assets/lottie/Confetti.jsx
+++ src/assets/lottie/Confetti.jsx
@@ -5,7 +5,12 @@ const Confetti = props => {
     const { width } = props;
     const style = {
         width: width,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
     };
+
     return (
         <div style={style}>
             <Lottie animationData={src} />
