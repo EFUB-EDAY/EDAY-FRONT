39b452d362ad36a2681e104b8ca1e7dbc6febc06
서진 <dltjwls0529@gmail.com>
2023-08-05 16:23:15 +0900

    🐛 : PrivateRoute 코드 수정

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -4,7 +4,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
     // const isLogin = !!localStorage.getItem('accessToken');
     const isLogin = true;
-
     return isLogin ? <Outlet /> : <Navigate to='/login' />;
 };
 
